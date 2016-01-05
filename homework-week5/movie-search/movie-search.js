a16f545a467747b1ed28c51493d33ee4866dcf24
bradochazo <mu.brad@gmail.com>
2016-01-04 21:25:03 -0800

    Changing javascript into query

--- homework-week5/movie-search/movie-search.js
+++ homework-week5/movie-search/movie-search.js
@@ -16,14 +16,21 @@ $("#movie-search-form").submit(formSubmitted);
 // Handle the form submission: go to OMDB and get results
 function formSubmitted(event) {
   event.preventDefault();
+
+
   var url = "http://omdbapi.com/?s=" + $("#query").val();
   $.get(url, resultsReceived);
   var existingList = document.querySelectorAll("li");
   for (var i = 0; i < existingList.length; i++) {
     existingList[i].classList.add("hidden");
+
   }
-  form.reset();
+  //var form = document.querySelector("#movie-search-form");
+  // var $form = $("#movie-search-form");
+  // form.reset();
+  $('#movie-search-form').trigger("reset");
 }
+
 function resultsReceived(results) {
 
   // Try putting a debugger here and inspecting the results argument
@@ -37,27 +44,42 @@ function resultsReceived(results) {
   }
 
   function createListItem() {
-    var list = document.querySelector("#movies");
-    var listItem = document.createElement("li");
+    // var list = document.querySelector("#movies");
+    var $list = $("#movies");
+    // var listItem = document.createElement("li");
+    var $listItem = $("<li>");
 
-    listItem.classList.add("movie");
-    list.appendChild(listItem);
+    // listItem.classList.add("movie");
+    // list.appendChild(listItem);
+    $listItem.addClass("movie").appendTo($list);
 
-    var title = document.createElement("div");
-    title.classList.add("movie-title");
-    listItem.appendChild(title);
 
-    var link = document.createElement("a");
-    link.setAttribute("href", "http://www.imdb.com/title/" + results["Search"][i]["imdbID"]);
-    link.setAttribute("target", "_blank");
-    link.textContent = results["Search"][i]["Title"];
-    listItem.appendChild(link);
+    // var title = document.createElement("div");
+    var $title = $("<div>");
+    // title.classList.add("movie-title");
+    // listItem.appendChild(title);
+    $title.addClass("movie-title").appendTo($listItem);
 
-    var year = document.createElement("div");
-    year.classList.add("movie-release-date");
-    year.textContent = results["Search"][i]["Year"];
 
-    listItem.appendChild(year);
+
+    // var link = document.createElement("a");
+    var $link = $("<a>");
+    // link.setAttribute("href", "http://www.imdb.com/title/" + results["Search"][i]["imdbID"]);
+    $link.attr("href", "http://www.imdb.com/title/" + results["Search"][i]["imdbID"]);
+    // link.setAttribute("target", "_blank");
+    $link.attr("target", "_blank");
+    // link.textContent = results["Search"][i]["Title"];
+    $link.text(results["Search"][i]["Title"]);
+    // listItem.appendChild(link);
+    $listItem.append($link);
+
+    //var year = document.createElement("div");
+    var $year = $("<div>");
+    // year.classList.add("movie-release-date");
+    // year.textContent = results["Search"][i]["Year"];
+    $year.addClass("movie-release-date").text(results["Search"][i]["Year"]).appendTo($listItem);
+
+    // listItem.appendChild(year);
 
   }
 
